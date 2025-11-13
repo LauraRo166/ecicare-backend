391654ea8cabcde961f8978fb14718148b95ac0c
LauraRo166 <laura.rsanchez@mail.escuelaing.edu.co>
2025-11-13 13:31:35 -0500

    cors

--- src/main/java/edu/escuelaing/ecicare/config/CorsConfig.java
+++ src/main/java/edu/escuelaing/ecicare/config/CorsConfig.java
@@ -15,7 +15,10 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("*"));
+        config.setAllowedOrigins(Arrays.asList(
+                "https://tu-frontend.github.io", // producción
+                "http://localhost:5173" // desarrollo local
+        ));
         config.addAllowedHeader("*");
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setMaxAge(3600L);
